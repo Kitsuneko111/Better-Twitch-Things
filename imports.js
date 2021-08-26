5e65d3250420089d59e076c2d4adda47257dc90b
Timemaster111 <53912018+Timemaster111@users.noreply.github.com>
2021-08-26 20:59:21 +0100

    Update imports.js

--- imports.js
+++ imports.js
@@ -1,24 +1,24 @@
 const tmi = require("tmi.js");
 require("dotenv").config();
 const client = new tmi.Client({
-    channels: ["Timemaster69"],
-    options: {
-      debug: true,
-      messageLogLevel: "info",
-    },
-    connection: {
-      reconnect: true,
-      secure: true,
-    },
-    identity: {
-      username: "BetterTwitchThings111",
-      password: process.env.oauth,
-    },
-  });
-  let messageCache = {};
-
-  module.exports = {
-      messageCache: messageCache,
-      client: client,
-      tmi: tmi
-  }
+  channels: ["Timemaster69"],
+  options: {
+    debug: true,
+    messageLogLevel: "info",
+  },
+  connection: {
+    reconnect: true,
+    secure: true,
+  },
+  identity: {
+    username: "BetterTwitchThings111",
+    password: process.env.oauth,
+  },
+});
+let messageCache = {};
+console.log("efficiency check");
+module.exports = {
+  messageCache: messageCache,
+  client: client,
+  tmi: tmi,
+};
