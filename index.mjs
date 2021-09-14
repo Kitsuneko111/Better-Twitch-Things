d019f88b2ec118884ab46d439b33e3201a243a16
Timemaster111 <53912018+Timemaster111@users.noreply.github.com>
2021-09-14 21:49:49 +0100

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -2,10 +2,28 @@
 
 const { client, messageCache } = require("./imports");
 
-client.connect().catch(console.error);
+const Discord = require("discord.js");
+const discClient = new Discord.Client({
+  intents: ["GUILDS", "DIRECT_MESSAGES", "GUILD_MESSAGES"],
+});
 
-client.on("message", (channel, tags, message, self) => {
+const Database = require("sqlite-async");
+
+await Database.open("channels.db");
+
+const mode = "DEBUG";
+
+client.on("message", async (channel, tags, message, self) => {
   if (self) return;
+  console.log(tags.username);
+  if (tags.username == "timemaster_111" && message == "!!!test!!!") {
+    client.say(channel, "Listening in (you are not in followers only mode)");
+    const guild = await discClient.guilds.fetch("682863690047029248");
+    const debugChannel = await guild.channels.fetch("887425121697017948");
+    debugChannel.send(`received test message`);
+  }
+  /*
+  WIP
   if (message.startsWith(`@${client.username}`)) {
   } else {
     let user;
@@ -36,10 +54,58 @@ client.on("message", (channel, tags, message, self) => {
       highestMessages[1].messages.splice(0, 1);
     }
   }
+  */
+});
+
+client.on("action", async (channel, userstate, message, self) => {
+  if (self) return;
+  banState = 0;
+  if (!userstate.username) return;
+  if (
+    /(\.*[0-9]+|_+|-+)*h(o|0)(s|$|t){2}([0-9]+|_+|-+)*.*/.test(
+      userstate.username.toLowerCase()
+    )
+  ) {
+    banState = 2;
+    if (/.*(o|0){2}3(1|i|l)2.*/.test(userstate.username.toLowerCase())) {
+      banState = 3;
+    }
+  }
+  if (banState >= 2 && mode != "DEBUG") {
+    await client.whisper(
+      userstate.username,
+      "Your name has been banned for having connections with a blocked username. In this case your username has been associated with IP grabber of username HOSS00312." +
+        `${
+          banState == 3
+            ? ""
+            : "If you believe this is in error you can appeal the ban."
+        }`
+    );
+    client.ban(
+      channel,
+      userstate.username,
+      "username assosciated with HOSS00312"
+    );
+  } else if (mode == "DEBUG") {
+    const guild = await discClient.guilds.fetch("682863690047029248");
+    const debugChannel = await guild.channels.fetch("887425121697017948");
+    debugChannel.send(
+      `${userstate.username} registered with hoss account (event type${message})`
+    );
+  }
+});
+
+discClient.on("ready", async () => {
+  const guild = await discClient.guilds.fetch("682863690047029248");
+  const debugChannel = await guild.channels.fetch("887425121697017948");
+  debugChannel.send(`ready`);
 });
 
 question("").then(() => {
   console.log("ending");
   client.disconnect();
+  discClient.disconnect();
   process.exit();
 });
+client.connect().catch(console.error);
+discClient.login(process.env.TOKEN);
